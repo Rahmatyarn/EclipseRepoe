a4912703eba522065b50b8771f157c2f1e1dd4ba
Naqib <naqibullah.rahmatyar@yahoo.com>
2022-11-10 21:08:10 -0500

    Commit from New Branch.

---
+++ src/NewBranch.java
@@ -0,0 +1,22 @@
+
+public class NewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		System.out.println("This is new branch");
+		
+	}
+
+}
